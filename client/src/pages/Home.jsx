9a4d480ce5609ebe52a65e9fa76612b87870e3e5
poredasite <poredasite@proton.me>
2026-04-18 01:01:28 +0300

    m

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -134,7 +134,7 @@ export default function Home() {
 
         {!loading && !error && videos.length > 0 && (
           <>
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3 animate-fade-in">
+            <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3 animate-fade-in">
               {gridItems.map((item, idx) =>
                 item.type === "ad"
                   ? <InFeedAd key={item.key} />
