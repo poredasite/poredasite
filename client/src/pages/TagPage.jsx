d92a53109aeeda2e3b07d7ab5d250abb1907970a
poredasite <poredasite@proton.me>
2026-04-27 18:03:44 +0300

    m

--- client/src/pages/TagPage.jsx
+++ client/src/pages/TagPage.jsx
@@ -113,7 +113,7 @@ export default function TagPage() {
         url={maxPage > 1 ? `${tagBase}?page=${maxPage}` : tagBase}
         prevPage={prevPage}
         nextPage={nextPage}
-        noIndex={true}
+        noIndex={maxPage > 1}
       />
 
       <div className="max-w-[1600px] mx-auto px-2 sm:px-5 py-4">
